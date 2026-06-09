c17ebf71bbaafbe68de418e87317e59518bfb789
Gus Young <76542590+gus-young@users.noreply.github.com>
2026-06-08 22:10:31 -0400

    ADDED - function that adds overfitting bool column to the df

--- analysis/cleaner.py
+++ analysis/cleaner.py
@@ -8,3 +8,11 @@ def filter_completed(df):
 def drop_incomplete_metrics(df):
     no_null = df.dropna(subset=["val_accuracy"])
     return no_null
+
+def normalize_model_type(df):
+    normalized_df = df["model_type"].str.strip().str.lower()
+    return normalized_df
+
+def flag_ovefitting(df, threshold=0.05):
+    return df['is_overfit'] = df['train_accuracy'] - df ['val_accuracy'] > threshold
+     
